126b140e8c70355fdb840089342f1662f216ad87
webobite <singhsubham510@gmail.com>
2021-05-30 23:20:32 +0530

    Final configuration done with template

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import './style.css'
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
+import { ClickCounter } from './ClickCounter'
+
 export const App = () => {
-    return <h1>React Typescript Webpack Starter Template</h1>
+  return (
+    <>
+      <h1>
+        Edited Hello React Typescript Webpack Starter Template -{' '}
+        {process.env.NODE_ENV} - {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React Logo" width="300" height="200" />
+      <img src={LOGO} alt="React Logo" width="300" height="300" />
+      <ClickCounter />
+    </>
+  )
 }
